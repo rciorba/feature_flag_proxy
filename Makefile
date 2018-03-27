a5932e43474321c4b301766fdb00fcfa71ebc474
Radu Ciorba <radu@devrandom.ro>
2018-03-27 13:33:28 +0300

    hello world

---
+++ Makefile
@@ -0,0 +1,11 @@
+PROJECT = feature_flag_proxy
+PROJECT_DESCRIPTION = Feature Flag Proxy
+PROJECT_VERSION = 0.1.0
+
+
+DEPS = cowboy gun
+dep_cowboy_commit = 2.2.2
+
+DEP_PLUGINS = cowboy
+
+include erlang.mk
---
+++ src/feature_flag_proxy_app.erl
@@ -0,0 +1,18 @@
+-module(feature_flag_proxy_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", proxy_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8081}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    feature_flag_proxy_sup:start_link().
+
+stop(_State) ->
+    ok.
