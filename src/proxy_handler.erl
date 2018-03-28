58f33c61b6ea8fb76857d177e2e55220b3007fa5
Radu Ciorba <radu@devrandom.ro>
2018-03-28 17:43:02 +0300

    implement selective request routing, based on regex

--- src/proxy_handler.erl
+++ src/proxy_handler.erl
@@ -19,22 +19,23 @@ receive_data(ConnPid, MRef, StreamRef, Accumulator) ->
             error_logger:error_msg("Oops!"),
             exit(Reason);
         Any ->
-            io:format("rd: ~p~n", [Any])
+            io:format("rd: ~p~n", [Any]),
+            exit(unexpected_msg)
     after 10000 ->
         exit(timeout)
     end.
 
 
 do_request(ConnPid, MRef, Path, Method, RequestHeaders, Body) ->
-    io:format("Method:~p~nPath:~p~nBody:~p~nHeaders:~p~n", [Method, Path, Body, RequestHeaders]),
+    %% io:format("Method:~p~nPath:~p~nBody:~p~nHeaders:~p~n", [Method, Path, Body, RequestHeaders]),
     StreamRef = case Body of
                     null -> gun:request(ConnPid, Method, Path, RequestHeaders);
                     _ -> gun:request(ConnPid, Method, Path, RequestHeaders, Body)
                 end,
-    io:format("StreamRef:~p~n", [StreamRef]),
+    %% io:format("StreamRef:~p~n", [StreamRef]),
     receive
         {gun_response, ConnPid, StreamRef, fin, Status, Headers} ->
-            io:format("dr-nofin:~p~n", [Status]),
+            %% io:format("dr-nofin:~p~n", [Status]),
             {ok, Status, Headers, <<"">>};
         {gun_response, ConnPid, StreamRef, nofin, Status, Headers} ->
             {ok, Status, Headers, receive_data(ConnPid, MRef, StreamRef)};
@@ -53,7 +54,7 @@ do_request(ConnPid, MRef, Path, Method, RequestHeaders, Body) ->
 
 
 transform_request_headers(Map) ->
-    [{K, binary:bin_to_list(V)} || 
+    [{K, binary:bin_to_list(V)} ||
         {K, V} <- filter_proxy_headers(maps:to_list(Map))
     ].
 
@@ -80,20 +81,40 @@ fix_header_name(HeaderName) ->
 
 transform_response_headers(Headers) ->
     %% TODO: https://www.mnot.net/blog/2011/07/11/what_proxies_must_do
-    [{<<"via">>, <<"1.1 feature_flag_proxy">>} | 
+    [{<<"via">>, <<"1.1 feature_flag_proxy">>} |
      [{fix_header_name(K), V} || {K, V} <- filter_proxy_headers(Headers)]
     ].
+
+
+match_server(Path, [], Default) ->
+    Default;
+match_server(Path, RouteSpec, Default) ->
+    [{Regexp, Server} | Tail] = RouteSpec,
+    case re:run(Path, Regexp) of
+        {match, _} -> Server;
+        nomatch -> match_server(Path, Tail, Default)
+    end.
+
+
+open_connection(Path) ->
+    RouteSpec = [
+                 {<<"^/api/v1/samples">>, {"localhost", 9090}}
+                ],
+    {Host, Port} = match_server(Path, RouteSpec, {"localhost", 8080}),
+    io:format("~p - ~p~n", [{Host, Port}, Path]),
+    {ok, ConnPid} = gun:open(Host, Port),
+    {ok, _Protocol} = gun:await_up(ConnPid),
+    MRef = monitor(process, ConnPid),
+    {ok, ConnPid, MRef}.
 
 
 init(Req, State) ->
     Path = iolist_to_binary(
              [<<"/">> | lists:join(<<"/">>, cowboy_req:path_info(Req))]),
-    io:format("~p~n", [Path]),
-    {ok, ConnPid} = gun:open("localhost", 8080),
-    {ok, Protocol} = gun:await_up(ConnPid),
-    MRef = monitor(process, ConnPid),
+    %% io:format("~p~n", [Path]),
+    {ok, ConnPid, MRef} = open_connection(Path),
     RequestHeaders = transform_request_headers(maps:get(headers, Req)),
-    io:format("RequestHeaders: ~p~n", [RequestHeaders]),
+    %% io:format("RequestHeaders: ~p~n", [RequestHeaders]),
     {ok, Body, Req0} = case cowboy_req:has_body(Req) of
                            true -> cowboy_req:read_body(Req);
                            false -> {ok, null, Req}
@@ -106,7 +127,7 @@ init(Req, State) ->
                                                 RequestHeaders,
                                                 Body),
     Headers = transform_response_headers(OriginalHeaders),
-    io:format("ResponseHeaders: ~p~nStatus:~p~nBody:~p~n", [Headers, Status, ResponseBody]),
+    %% io:format("ResponseHeaders: ~p~nStatus:~p~nBody:~p~n", [Headers, Status, ResponseBody]),
     Req1 = cowboy_req:reply(Status,
         %% #{<<"content-type">> => <<"text/html">>},
         maps:from_list(Headers),
