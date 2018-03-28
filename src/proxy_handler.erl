1135b2218379975c7943a0e95566e7afcee20a8f
Radu Ciorba <radu@devrandom.ro>
2018-03-28 16:45:30 +0300

    handle requests with a body

--- src/proxy_handler.erl
+++ src/proxy_handler.erl
@@ -8,6 +8,7 @@ receive_data(ConnPid, MRef, StreamRef) ->
     Data = receive_data(ConnPid, MRef, StreamRef, []),
     iolist_to_binary(lists:reverse(Data)).
 
+
 receive_data(ConnPid, MRef, StreamRef, Accumulator) ->
     receive
         {gun_data, ConnPid, StreamRef, nofin, Data} ->
@@ -24,9 +25,12 @@ receive_data(ConnPid, MRef, StreamRef, Accumulator) ->
     end.
 
 
-do_request(ConnPid, MRef, Path, Method, RequestHeaders) ->
-    io:format("~p~n", [Method]),
-    StreamRef = gun:request(ConnPid, Method, Path, RequestHeaders),
+do_request(ConnPid, MRef, Path, Method, RequestHeaders, Body) ->
+    io:format("Method:~p~nPath:~p~nBody:~p~nHeaders:~p~n", [Method, Path, Body, RequestHeaders]),
+    StreamRef = case Body of
+                    null -> gun:request(ConnPid, Method, Path, RequestHeaders);
+                    _ -> gun:request(ConnPid, Method, Path, RequestHeaders, Body)
+                end,
     io:format("StreamRef:~p~n", [StreamRef]),
     receive
         {gun_response, ConnPid, StreamRef, fin, Status, Headers} ->
@@ -35,7 +39,8 @@ do_request(ConnPid, MRef, Path, Method, RequestHeaders) ->
         {gun_response, ConnPid, StreamRef, nofin, Status, Headers} ->
             {ok, Status, Headers, receive_data(ConnPid, MRef, StreamRef)};
         {gun_error, ConnPid, StreamRef, Error} ->
-            error_logger:error_msg("Oops!"),
+            error_logger:error_msg("Cacao!"),
+            io:format("dr_error:~p~n", [Error]),
             exit(Error);
         {'DOWN', MRef, process, ConnPid, Reason} ->
             error_logger:error_msg("Oops!"),
@@ -47,23 +52,14 @@ do_request(ConnPid, MRef, Path, Method, RequestHeaders) ->
     end.
 
 
-cowboy_to_gun_headers(Map) ->
-    [{K, binary:bin_to_list(V)} || {K, V} <- maps:to_list(Map),
-                                   K =/= <<"connection">>
+transform_request_headers(Map) ->
+    [{K, binary:bin_to_list(V)} || 
+        {K, V} <- filter_proxy_headers(maps:to_list(Map))
     ].
 
 
-fix_header_name(HeaderName) ->
-    case HeaderName of
-        <<"set-cookie">> -> <<"Set-Cookie">>;
-        Any -> Any
-    end.
-
-
-transform_response_headers(Headers) ->
-    %% TODO: https://www.mnot.net/blog/2011/07/11/what_proxies_must_do
-    [{fix_header_name(Name), Value} ||
-        {Name, Value} <- Headers,
+filter_proxy_headers(Headers) ->
+    [{Name, Value} || {Name, Value} <- Headers,
         Name =/= <<"connection">>,
         Name =/= <<"content-length">>,
         Name =/= <<"te">>,
@@ -75,32 +71,46 @@ transform_response_headers(Headers) ->
         Name =/= <<"upgrade">>
     ].
 
+fix_header_name(HeaderName) ->
+    case HeaderName of
+        <<"set-cookie">> -> <<"Set-Cookie">>;
+        Any -> Any
+    end.
 
-init(Req0, State) ->
+
+transform_response_headers(Headers) ->
+    %% TODO: https://www.mnot.net/blog/2011/07/11/what_proxies_must_do
+    [{<<"via">>, <<"1.1 feature_flag_proxy">>} | 
+     [{fix_header_name(K), V} || {K, V} <- filter_proxy_headers(Headers)]
+    ].
+
+
+init(Req, State) ->
+    Path = iolist_to_binary(
+             [<<"/">> | lists:join(<<"/">>, cowboy_req:path_info(Req))]),
+    io:format("~p~n", [Path]),
     {ok, ConnPid} = gun:open("localhost", 8080),
     {ok, Protocol} = gun:await_up(ConnPid),
     MRef = monitor(process, ConnPid),
-    Path = iolist_to_binary(
-             [<<"/">> | lists:join(<<"/">>, cowboy_req:path_info(Req0))]),
-    io:format("~p~n", [Path]),
-    RequestHeaders = cowboy_to_gun_headers(maps:get(headers, Req0)),
+    RequestHeaders = transform_request_headers(maps:get(headers, Req)),
     io:format("RequestHeaders: ~p~n", [RequestHeaders]),
-    {ok, Status, OriginalHeaders, Response} = do_request(
+    {ok, Body, Req0} = case cowboy_req:has_body(Req) of
+                           true -> cowboy_req:read_body(Req);
+                           false -> {ok, null, Req}
+                       end,
+    {ok, Status, OriginalHeaders, ResponseBody} = do_request(
                                                 ConnPid,
                                                 MRef,
                                                 Path,
                                                 maps:get(method, Req0),
-                                                RequestHeaders),
-    Headers = [
-               {<<"via">>, <<"1.1 feature_flag_proxy">>}
-               |
-               transform_response_headers(OriginalHeaders)
-              ],
-    io:format("Headers: ~p~n", [Headers]),
-    Req = cowboy_req:reply(Status,
+                                                RequestHeaders,
+                                                Body),
+    Headers = transform_response_headers(OriginalHeaders),
+    io:format("ResponseHeaders: ~p~nStatus:~p~nBody:~p~n", [Headers, Status, ResponseBody]),
+    Req1 = cowboy_req:reply(Status,
         %% #{<<"content-type">> => <<"text/html">>},
         maps:from_list(Headers),
-        Response,
+        ResponseBody,
         Req0),
     gun:shutdown(ConnPid),
-    {ok, Req, State}.
+    {ok, Req1, State}.
