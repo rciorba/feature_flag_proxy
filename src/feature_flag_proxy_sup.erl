b1d3db33645b951ec15f1382709d98418bc4b991
Radu Ciorba <radu@devrandom.ro>
2018-04-12 16:23:16 +0300

    indentaion

--- src/feature_flag_proxy_sup.erl
+++ src/feature_flag_proxy_sup.erl
@@ -5,15 +5,15 @@
 -export([init/1]).
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [
-            {route_srv,
-             {route_spec_server, start_link, []},
-             permanent,
-             1,
-             worker,
-             [route_spec_server]}
-        ],
-	{ok, {{one_for_one, 100, 5}, Procs}}.
+    Procs = [
+             {route_srv,
+              {route_spec_server, start_link, []},
+              permanent,
+              1,
+              worker,
+              [route_spec_server]}
+            ],
+    {ok, {{one_for_one, 100, 5}, Procs}}.
